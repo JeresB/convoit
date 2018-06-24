d0ff005b2ede20d744bc4f4b85988664337bce49
Jeremy <boiselet.jeremy@gmail.com>
2018-06-24 12:12:04 +0200

    *

--- src/FrontOfficeBundle/Controller/DefaultController.php
+++ src/FrontOfficeBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace FrontOfficeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
