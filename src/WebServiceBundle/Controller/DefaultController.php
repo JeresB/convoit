fa1036932f6044bf1b8053b248b88d278456c433
Jeremy <boiselet.jeremy@gmail.com>
2018-06-25 13:19:26 +0200

    *

--- src/WebServiceBundle/Controller/DefaultController.php
+++ src/WebServiceBundle/Controller/DefaultController.php
@@ -30,14 +30,15 @@ class DefaultController extends Controller
     {
       $em = $this->getDoctrine()->getManager();
 
-      $query = $em->createQuery("SELECT t.id, t.nbKm, t.date, IDENTITY(t.internauteId) as internaute, vd.ville as depart, va.ville as arrivee
+      $query = $em->createQuery("SELECT t.id, t.nbKm, t.date, CONCAT(i.prenom, ' ', i.nom) AS internaute, vd.ville as depart, va.ville as arrivee
         FROM BackOfficeBundle:Trajet t,
+        BackOfficeBundle:internaute i,
         BackOfficeBundle:Ville v,
         BackOfficeBundle:Ville vd,
         BackOfficeBundle:Ville va
         WHERE ((v.ville LIKE :search AND t.villeId = v.id)
         OR (v.ville LIKE :search AND t.villeId1 = v.id))
-        AND vd.id = t.villeId AND va.id = t.villeId1")
+        AND vd.id = t.villeId AND va.id = t.villeId1 AND i.id = t.internauteId")
         ->setParameter('search', '%'.$search.'%');
 
       $trajets = $query->getResult();
