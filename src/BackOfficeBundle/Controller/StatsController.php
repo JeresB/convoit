747238601b953d1158c192edc016daedc5b993e4
Jeremy <boiselet.jeremy@gmail.com>
2018-06-25 15:47:16 +0200

    *

--- src/BackOfficeBundle/Controller/StatsController.php
+++ src/BackOfficeBundle/Controller/StatsController.php
@@ -15,13 +15,25 @@ class StatsController extends Controller
         $resultat = $query->getResult();
         $nb_internaute = $resultat[0]['total'];
 
-        $query = $em->createQuery("SELECT count(t.internauteId) Total FROM BackOfficeBundle:Trajet t group by t.internauteId having count(t.internauteId) > 2");
+        $query = $em->createQuery("SELECT count(t.internauteId) total FROM BackOfficeBundle:Trajet t group by t.internauteId having count(t.internauteId) > 2");
 
         $resultat = $query->getResult();
         $nb_internaute_trajet_2 = $resultat;
 
+        $query = $em->createQuery("SELECT t.villeId, count(t) total FROM BackOfficeBundle:Trajet t group by t.villeId order by total desc limit 5");
+
+        $resultat = $query->getResult();
+        $top_depart = $resultat;
+
+        $query = $em->createQuery("SELECT t.villeId1, count(t) total FROM BackOfficeBundle:Trajet t group by t.villeId1 order by total desc limit 5");
+
+        $resultat = $query->getResult();
+        $top_arrivee = $resultat;
+
         return $this->render('BackOfficeBundle:Default:stats.html.twig',
           array("nb_internaute" => $nb_internaute,
-                "nb_internaute_trajet_2" => $nb_internaute_trajet_2));
+                "nb_internaute_trajet_2" => $nb_internaute_trajet_2,
+                "top_depart" => $top_depart,
+                "top_arrivee" => $top_arrivee));
     }
 }
