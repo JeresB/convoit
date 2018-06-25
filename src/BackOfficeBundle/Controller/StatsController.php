1ad56df3ba385aa42c63f4cb74b821aed9c52cc7
Jeremy <boiselet.jeremy@gmail.com>
2018-06-25 08:19:07 +0200

    *

---
+++ src/BackOfficeBundle/Controller/StatsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BackOfficeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class StatsController extends Controller
+{
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery("SELECT count(*) as total FROM BackOfficeBundle:internaute");
+
+        $resultat = $query->getResult();
+
+        return $this->render('BackOfficeBundle:Default:stats.html.twig',
+          array("nb_internaute" => $resultat));
+    }
+}
