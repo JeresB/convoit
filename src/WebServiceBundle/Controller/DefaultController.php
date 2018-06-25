dc3af3b8f21e807becb063fa3d76dc187df4c492
Jeremy <boiselet.jeremy@gmail.com>
2018-06-25 15:03:15 +0200

    *

--- src/WebServiceBundle/Controller/DefaultController.php
+++ src/WebServiceBundle/Controller/DefaultController.php
@@ -55,7 +55,7 @@ class DefaultController extends Controller
         BackOfficeBundle:Ville v,
         BackOfficeBundle:Ville vd,
         BackOfficeBundle:Ville va
-        WHERE t.i = :id AND vd.id = t.villeId AND va.id = t.villeId1 AND i.id = t.internauteId")
+        WHERE t.id = :id AND vd.id = t.villeId AND va.id = t.villeId1 AND i.id = t.internauteId")
         ->setParameter('id', $id);
 
       $trajets = $query->getResult();
