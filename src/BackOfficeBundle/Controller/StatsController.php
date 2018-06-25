d897e1e089f704666696efc7ebeafc226b1c0dd6
Jeremy <boiselet.jeremy@gmail.com>
2018-06-25 15:19:36 +0200

    *

--- src/BackOfficeBundle/Controller/StatsController.php
+++ src/BackOfficeBundle/Controller/StatsController.php
@@ -13,8 +13,14 @@ class StatsController extends Controller
         $query = $em->createQuery("SELECT count(i.id) as total FROM BackOfficeBundle:internaute i");
 
         $resultat = $query->getResult();
+        $nb_internaute = $resultat[0]['total'];
+
+        $query = $em->createQuery("SELECT internaute_id, count(*) Total FROM BackOfficeBundle:Trajet t group by t.internaute_id having count(*) > 2");
+
+        $resultat = $query->getResult();
 
         return $this->render('BackOfficeBundle:Default:stats.html.twig',
-          array("nb_internaute" => $resultat[0]['total']));
+          array("nb_internaute" => $nb_internaute,
+                "nb_internaute_trajet_2" => $resultat));
     }
 }
