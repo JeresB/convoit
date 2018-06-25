16e4cc337c9a4204901ad209ae854c5aa38ea746
Jeremy <boiselet.jeremy@gmail.com>
2018-06-25 12:54:29 +0200

    *

--- src/WebServiceBundle/Controller/DefaultController.php
+++ src/WebServiceBundle/Controller/DefaultController.php
@@ -13,7 +13,16 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-        $trajets = $em->getRepository('BackOfficeBundle:Trajet')->findAll();
+
+        $query = $em->createQuery("SELECT t.id, t.nbKm, t.date, CONCAT(i.prenom, ' ', i.nom) AS internaute, vd.ville as depart, va.ville as arrivee
+          FROM BackOfficeBundle:Trajet t,
+          BackOfficeBundle:internaute i,
+          BackOfficeBundle:Ville vd,
+          BackOfficeBundle:Ville va
+          WHERE vd.id = t.villeId AND va.id = t.villeId1 AND t.internauteId = i.id");
+
+        $trajets = $query->getResult();
+      //  $trajets = $em->getRepository('BackOfficeBundle:Trajet')->findAll();
 
         //return $this->json(array('trajets' => $trajets));
         return $this->render('WebServiceBundle:Default:index.html.twig', array("trajets" => $trajets));
